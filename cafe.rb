fbb1d993a76904562a6191885934a336e5dda4b4
shin-fkmt <fkmt.shin@gmail.com>
2026-03-03 14:21:03 +0900

    ドリンクとフードの注文番号を格納する変数名を適切な変数名に修正

--- cafe.rb
+++ cafe.rb
@@ -25,10 +25,10 @@ def take_order(menus)
 end
 
 puts 'bugカフェへようこそ！ご注文は？ 番号でどうぞ'
-order1 = take_order(DRINKS)
+drink_order_number = take_order(DRINKS)
 
 puts 'フードメニューはいかがですか?'
-order2 = take_order(FOODS)
+food_order_number = take_order(FOODS)
 
-total = DRINKS[order1][:price].to_i + FOODS[order2][:price].to_i
+total = DRINKS[drink_order_number][:price].to_i + FOODS[food_order_number][:price].to_i
 puts "お会計は#{total}円になります。ありがとうございました！"
